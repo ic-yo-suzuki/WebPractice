89d7d78c3d126f927d40c98881ca913384b1e47e
ic-yo-suzuki <yo_suzuki@ic-com.co.jp>
2016-04-15 17:56:14 +0900

    Chapter4 コード修正→request.setCharcterEncoding("UTF-8);を追加(InputsServlet.java)

--- Chapter4/src/chapter4/InputsServlet.java
+++ Chapter4/src/chapter4/InputsServlet.java
@@ -18,6 +18,7 @@ public class InputsServlet extends HttpServlet{
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 
 		Map<String, String[]> parameterMap = request.getParameterMap();
