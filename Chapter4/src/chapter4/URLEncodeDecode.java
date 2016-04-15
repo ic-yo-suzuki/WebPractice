ce47383340ecc528f557cdcd70466fadd410b037
ic-yo-suzuki <yo_suzuki@ic-com.co.jp>
2016-04-15 17:37:09 +0900

    Chapter4 アノテーション URLエンコード・デコード 入出力

---
+++ Chapter4/src/chapter4/URLEncodeDecode.java
@@ -0,0 +1,23 @@
+package chapter4;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+public class URLEncodeDecode {
+	private static final String ENC = "UTF-8";
+
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		String hello = URLEncoder.encode("こんにちは", ENC);
+		String japan = URLEncoder.encode("日本", ENC);
+		String myBook = URLEncoder.encode("Eclipseで学ぶ初めてのJava", ENC);
+
+		System.out.println(hello);
+		System.out.println(japan);
+		System.out.println(myBook);
+
+		System.out.println(URLDecoder.decode(hello, ENC));
+		System.out.println(URLDecoder.decode(japan, ENC));
+		System.out.println(URLDecoder.decode(myBook, ENC));
+	}
+}
